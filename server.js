6853ebd6a8a9387395413cd01df77581eccf110d
Tali <taliitacamargo@icloud.com>
2021-11-17 16:15:23 -0500

    update

--- server.js
+++ server.js
@@ -4,35 +4,53 @@ const express = require('express');
 const path = require('path');
 // writefile method
 const fs = require('fs');
-// 
-// const api = require('./routes/')
+const api = require('./routes/apiroutes');
+const htmlroutes = require ('./routes/htmlroutes');
+const db = require('./db/db.json');
 
 // defining port variable
-const PORT = 3001;
+const PORT = process.env.port || 3001;
 // creating server object
 const app = express();
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use('/api', api);
+app.use('/api', api);
+
 // determines where the supporting assets are going to be pulled from 
 app.use(express.static('public'));
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
 
-// GET Route for feedback page
+
+// GET Route for notes.html page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+// GET route for reading the db.json file 
+app.get('/api/notes', (req, res) =>
+  res.json('/db/db.json')
+);
+// GET Route for index.html page
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
+miniapp.post('/api/notes', (req, res) => {
+    const {jsonNotes} = req.body;
+    if (req.body) {
+       const newJsonNotes = {
+        notes,
+       };
+       db.push(newJsonNotes);
+       res.json(db);  
+    } else {
+        res.error('Error in adding new notes');
+    }
+});
 
-
-
-
+// listens to port
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
