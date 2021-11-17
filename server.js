a195d6b8f71f02f4866e073778b89576ea4eea1f
Tali <taliitacamargo@icloud.com>
2021-11-17 14:19:34 -0500

    making changes to server.js file (adding/removing routes, editing middeware, adding listen to port

--- server.js
+++ server.js
@@ -1,13 +1,38 @@
+// importing express package for the server 
 const express = require('express');
+// importing file path resolution methods
 const path = require('path');
+// writefile method
 const fs = require('fs');
-const api = require('./routes/')
+// 
+// const api = require('./routes/')
 
+// defining port variable
 const PORT = 3001;
+// creating server object
 const app = express();
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+// app.use('/api', api);
+// determines where the supporting assets are going to be pulled from 
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for feedback page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
 
+
+
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
